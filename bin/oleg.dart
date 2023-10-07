c32bed5c9900f396345bcc6d1fb49c2df947fc3a
karga <karga>
2023-10-07 10:38:38 +0500

    aaa

--- bin/oleg.dart
+++ bin/oleg.dart
@@ -1,5 +1,6 @@
 import 'package:oleg/oleg.dart' as oleg;
 import 'package:dio/dio.dart';
+import 'models/cars/cars/cars.dart';
 import 'models/cars/carsmodel/carsmodel.dart';
 
 void main(List<String> arguments) async {
@@ -8,5 +9,14 @@ void main(List<String> arguments) async {
   var response = await httpClient.get(url);
   Carsmodel carsmodel = Carsmodel.fromJson(response.data);
 
-  
+  int count = 0;
+  int carprice = 0;
+  List<Cars> Yellow = data.cars;
+  for(Cars car in Yellow){
+    if(car.car_color == 'Yellow'){
+      String carprice = car.price.substring(1);
+      count ++; 
+    }
+  }  
+  print(carprice/count);
 }
